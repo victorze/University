9a047a2c95184c8f86ee9ecdf8b1fc2fd96a33a8
Victor Hugo <victorze@protonmail.com>
2022-08-23 21:31:12 -0500

    Add user model and user migration

--- UniversityApi/DataAcess/UniversityDBContext.cs
+++ UniversityApi/DataAcess/UniversityDBContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using UniversityApi.Models;
 
 namespace UniversityApi.DataAcess
 {
     public class UniversityDBContext : DbContext
     {
         public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) { }
+
+        public DbSet<User>? Users { get; set; }
     }
 }
